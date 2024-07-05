fc120a7cda47bf6605ce32ef3a76dfe34326bc07
Ivan <user-email@go.uk>
2024-07-05 21:24:30 +0300

    fix gitignore api todo [9]

--- src/database.py
+++ src/database.py
@@ -2,8 +2,9 @@ from fastapi import HTTPException
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///data.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=False)
+#SQLALCHEMY_DATABASE_URL = "sqlite:///data.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://data_todo_user:KKk7fNOtD2gq9yNtcVGJuLNWYfhNXlTQ@dpg-cq3a4jjqf0us73ddlnq0-a.oregon-postgres.render.com/data_todo"
+engine = create_engine(SQLALCHEMY_DATABASE_URL,  echo=False) #connect_args={"check_same_thread": False},
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
