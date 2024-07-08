5b44060d9cd634257d422e78938862b1aefe0d7f
Ivan <user-email@go.uk>
2024-07-08 21:05:57 +0300

    refactor get current user for  protect route and  [12]

--- src/routes/auth_views.py
+++ src/routes/auth_views.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 
 from ..database import get_db
 from ..schemas.user import User as UserSchema, UserCreate as UserCreateSchema, UserLogin, User
-from ..services.auth import create_access_token, get_current_user_id
+from ..services.auth import create_access_token, get_current_user_id, get_current_user
 from ..services.crud_user import RepoUsers
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
@@ -32,7 +32,6 @@ def get_users(db: Session = Depends(get_db)):
     return users
 
 
-
 @router.post("/login")
 def login_user(payload: UserLogin, db: Session = Depends(get_db)):
     """
@@ -55,10 +54,13 @@ def read_users_me(db: Session = Depends(get_db), current_user_id: int = Depends(
     """
 
     current_user = RepoUsers.get_user_by_id(db=db, id=current_user_id)
+    return current_user
+
+
+@router.get("/users/me2", response_model=UserSchema)
+def read_users_me2(current_user: User = Depends(get_current_user)):
     if not current_user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found",
-        )
+            detail="User not found")
     return current_user
-
--- src/services/auth.py
+++ src/services/auth.py
@@ -8,14 +8,15 @@ from .crud_user import RepoUsers
 from ..database import get_db
 from sqlalchemy.orm import Session
 
+
 def create_access_token(subject: str, expires_delta: int = None) -> str:
     if expires_delta is not None:
-        expires_delta = datetime.utcnow() + expires_delta
+        expire = datetime.utcnow() + expires_delta
     else:
-        expires_delta = datetime.utcnow() + timedelta(minutes=setting.ACCESS_TOKEN_EXPIRE_MINUTES)
-
+        expire = datetime.utcnow() + timedelta(minutes=setting.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode = {
-        "exp": expires_delta,
+        "exp": expire,
+        "expire": (expire + timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S"),
         "sub": subject,
         "iat": datetime.utcnow(),
         "iss": "api_restfull:todo"}
@@ -53,6 +54,28 @@ def get_current_user_id(credentials: HTTPAuthorizationCredentials = Depends(HTTP
     return user_id
 
 
+def get_current_user(
+        credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer()),
+        db: Session = Depends(get_db)
+):
+    """
+    Get current user obj from JWT token
+    """
+    credentials_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token or expired token",
+    )
+    token = credentials.credentials
+    payload = decode_token(token)
+    if not payload:
+        raise credentials_exc
+    current_user_id = payload.get("sub")
+    if not current_user_id:
+        raise credentials_exc
+    current_user = RepoUsers.get_user_by_id(db=db, id=current_user_id)
+    return current_user
+
+
 
 # def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
 #     if expires_delta is not None:
@@ -63,4 +86,3 @@ def get_current_user_id(credentials: HTTPAuthorizationCredentials = Depends(HTTP
 #     to_encode = {"exp": expires_delta, "sub": str(subject)}
 #     encoded_jwt = jwt.encode(to_encode, setting.refresh_secret_key, setting.algorithm)
 #     return encoded_jwt
-
